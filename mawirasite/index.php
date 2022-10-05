a2d8f29c518bfb9737e5069d005469f08412a764
mawiraam <anggaitzve@gmail.com>
2022-10-05 23:33:25 +0700

    Upluad tugas 6 PHP

--- mawirasite/index.php
+++ mawirasite/index.php
@@ -94,67 +94,26 @@
     </section><!-- End About Section -->
 
     <!-- ======= Skills Section ======= -->
-    <section id="skills" class="skills section-bg">
-      <div class="container">
+    <section id="skills" class="about">
+    <div class="container">
 
-        <div class="section-title">
-          <h2>Skills</h2>
-        </div>
+<div class="section-title">
+  <h2>Pendidikan</h2>
+</div>
 
-        <div class="row skills-content">
-
-          <div class="col-lg-6" data-aos="fade-up">
-
-            <div class="progress">
-              <span class="skill">HTML <i class="val">100%</i></span>
-              <div class="progress-bar-wrap">
-                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-              </div>
-            </div>
-
-            <div class="progress">
-              <span class="skill">CSS <i class="val">90%</i></span>
-              <div class="progress-bar-wrap">
-                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
-              </div>
-            </div>
-
-            <div class="progress">
-              <span class="skill">JavaScript <i class="val">75%</i></span>
-              <div class="progress-bar-wrap">
-                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-              </div>
-            </div>
-
-          </div>
-
-          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-
-            <div class="progress">
-              <span class="skill">PHP <i class="val">80%</i></span>
-              <div class="progress-bar-wrap">
-                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-              </div>
-            </div>
-
-            <div class="progress">
-              <span class="skill">Photoshop <i class="val">50%</i></span>
-              <div class="progress-bar-wrap">
-                <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-              </div>
-            </div>
-            <div class="progress">
-              <span class="skill">Adobe Illustrator <i class="val">50%</i></span>
-              <div class="progress-bar-wrap">
-                <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-              </div>
-            </div>
-
-          </div>
-
-        </div>
-
+            <div class="row">
+              <div class="col-lg-6">
+        <ul>
+          <li><i class="bi bi-chevron-right"></i> <strong>SMP</strong> <span>SMPIT Raudhatul Jannah</span></li>
+          <li><i class="bi bi-chevron-right"></i> <strong>SMA</strong> <span>SMAIT Raudhatul Jannah</span></li>
+          <li><i class="bi bi-chevron-right"></i> <strong>Universitas</strong> <span>Universitas Serang Raya</span></li>
+        </ul>
       </div>
+    </div>
+  </div>
+</div>
+
+</div>
     </section><!-- End Skills Section -->
 
     <!-- ======= Portfolio Section ======= -->
@@ -162,7 +121,7 @@
       <div class="container">
 
         <div class="section-title">
-          <h2>Portfolio</h2>
+          <h2>Taks</h2>
           <p>Ini adalah beberapa project / tugas tugas yang saya kerjakan selama mengikuti program kampus merdeka</p>
         </div>
 
